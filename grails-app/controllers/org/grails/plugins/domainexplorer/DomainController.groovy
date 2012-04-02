f315a340c6845eefb8908f523424b4c9a29b9da1
Matt Sheehan <matt.sheehan@objectpartners.com>
2012-04-02 17:09:32 -0500

    some api calls

--- grails-app/controllers/org/grails/plugins/domainexplorer/DomainController.groovy
+++ grails-app/controllers/org/grails/plugins/domainexplorer/DomainController.groovy
@@ -2,6 +2,7 @@ package org.grails.plugins.domainexplorer
 
 import org.codehaus.groovy.grails.commons.GrailsDomainClass
 import grails.converters.JSON
+import org.codehaus.groovy.grails.validation.ConstrainedProperty
 
 class DomainController {
 
@@ -27,5 +28,54 @@ class DomainController {
             count: domainClass.clazz.count()
         ]
         render json as JSON
+    }
+
+    def listEntities() {
+        GrailsDomainClass domainClass = grailsApplication.getDomainClass(params.fullName)
+//        def json = domainClass.clazz.list().collect { entityToMap it, domainClass }
+        def json = domainClass.clazz.list()
+        render json as JSON
+    }
+
+    def domainType() {
+        GrailsDomainClass domainClass = grailsApplication.getDomainClass(params.fullName)
+        Map constrainedProperties = domainClass.constrainedProperties
+        Map json = [
+            name: domainClass.name,
+            fullName: domainClass.fullName,
+            properties: domainClass.properties.collect {
+                Map m = [
+                    name: it.name,
+                    type: it.referencedPropertyType,
+                    persistent: it.persistent,
+                    optional: it.optional,
+                    identity: it.identity,
+                    hasOne: it.hasOne,
+                    oneToOne: it.oneToOne,
+                    oneToMany: it.oneToMany,
+                    manyToOne: it.manyToOne,
+                    manyToMany: it.manyToMany,
+                    bidirectional: it.bidirectional,
+                    association: it.association,
+                    enum: it.enum,
+                    naturalName: it.naturalName,
+                    inherited: it.inherited,
+                    owningSide: it.owningSide,
+                    circular: it.circular,
+                    embedded: it.embedded,
+                    derived: it.derived,
+                ]
+                ConstrainedProperty cp = constrainedProperties[it.name]
+                if (cp) {
+                    m.constraints = cp.properties
+                }
+                m
+            }
+        ]
+        render json as JSON
+    }
+
+    private Map entityToMap(entity, GrailsDomainClass domainClass) {
+        entity as Map // TODO
     }
 }
